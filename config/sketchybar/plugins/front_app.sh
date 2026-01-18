609eb286b35a4c23993cce310a229ce7d0323821
gcodenowski <gilbertkozanowski@gmail.com>
2026-01-18 15:06:23 +0000

    Dotfiles update 18-Jan-2026_15-06-23

--- config/sketchybar/plugins/front_app.sh
+++ config/sketchybar/plugins/front_app.sh
@@ -1,14 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # == Front App ==
 sketchybar --add item frontapp left \
-           --set frontapp script="$PLUGIN_DIR/front_app.sh" \
+           --set frontapp script="$PLUGIN_DIR/front_app_switched.sh" \
   			  icon.background.drawing=on \
                           icon.background.image.scale=0.7 \
+			  icon.background.image.padding_left=5 \
            --subscribe frontapp front_app_switched
 
-# === Logic ===
-if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO" \
-			   icon.background.image="app.$INFO"
-fi
---
+++ config/sketchybar/plugins/front_app_switched.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# === Logic ===
+if [ "$SENDER" = "front_app_switched" ]; then
+  sketchybar --set "$NAME" label="$INFO" \
+                           icon.background.image="app.$INFO"
+fi
+~                
