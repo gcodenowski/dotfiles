d9a436a4e032d611fcb3a2e205cf18e2ea3f9311
gcodenowski <gilbertkozanowski@gmail.com>
2026-01-19 13:57:39 +0000

    Dotfiles update 19-Jan-2026_13-57-39

--- config/sketchybar/plugins/cpu.sh
+++ config/sketchybar/plugins/cpu.sh
@@ -21,8 +21,11 @@ sketchybar --add graph cpu_graph right 40 \
            --set cpu_graph graph.color=$LABEL_COLOR \
                            graph.fill_color=0x0000000 \
                            graph.line_width=1.5 \
+			   padding_left=0 \
+			   padding_right=0 \
                            background.height=30 \
                            background.corner_radius=10 \
+			   background.border_color=$CLEAR \
                            y_offset=0 \
                            update_freq=1 \
                            script="$PLUGIN_DIR/cpu_graph.sh" \
