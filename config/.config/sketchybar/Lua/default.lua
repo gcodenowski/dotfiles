f8008d361f81a1b27132efcb7c443beff73705a3
gcodenowski <gilbertkozanowski@gmail.com>
2026-02-02 23:14:24 +0000

    Added init.lua

--- config/.config/sketchybar/Lua/default.lua
+++ config/.config/sketchybar/Lua/default.lua
@@ -2,6 +2,32 @@ local colors = require("colors")
 
 -- Equivalent to the --default domain
 sbar.default({
+	padding_left = 2
+	padding_right = 2
+
 	icon = {
-		font = "SF Pro:Bold:17.0"
+		font = "SF Pro:Bold:17.0",
+		color = colors.light_blue,		
+		padding_left = 6,
+		padding_right = 0,
+		y_offset = 1
 		
+	}
+
+	label = {
+		font = "SF Pro:Bold:16:0",
+		font_smoothing = on,
+		color = colors.text,
+		padding_left = 4,
+		padding_right = 4
+	}
+
+	background = {
+		corner_radius = 5,
+		color = colors.bg,
+		height = 30,
+		border_color = colors.border,
+		border_width = 1,
+		blur_radius = 1,
+	}
+}
