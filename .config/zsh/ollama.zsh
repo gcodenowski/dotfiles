331e591ee2b416bd90f7889e25f99ce0b293a794
gcodenowski <gilbertkozanowski@gmail.com>
2026-03-10 21:50:47 +0000

    added ollama.zsh

---
+++ .config/zsh/ollama.zsh
@@ -0,0 +1,24 @@
+: <<'COMMENT'
+    OOOOOOOOO     lllllll lllllll                                                             
+   OO:::::::::OO   l:::::l l:::::l                                                             
+ OO:::::::::::::OO l:::::l l:::::l                                                             
+O:::::::OOO:::::::Ol:::::l l:::::l                                                             
+O::::::O   O::::::O l::::l  l::::l   aaaaaaaaaaaaa      mmmmmmm    mmmmmmm     aaaaaaaaaaaaa   
+O:::::O     O:::::O l::::l  l::::l   a::::::::::::a   mm:::::::m  m:::::::mm   a::::::::::::a  
+O:::::O     O:::::O l::::l  l::::l   aaaaaaaaa:::::a m::::::::::mm::::::::::m  aaaaaaaaa:::::a 
+O:::::O     O:::::O l::::l  l::::l            a::::a m::::::::::::::::::::::m           a::::a 
+O:::::O     O:::::O l::::l  l::::l     aaaaaaa:::::a m:::::mmm::::::mmm:::::m    aaaaaaa:::::a 
+O:::::O     O:::::O l::::l  l::::l   aa::::::::::::a m::::m   m::::m   m::::m  aa::::::::::::a 
+O:::::O     O:::::O l::::l  l::::l  a::::aaaa::::::a m::::m   m::::m   m::::m a::::aaaa::::::a 
+O::::::O   O::::::O l::::l  l::::l a::::a    a:::::a m::::m   m::::m   m::::ma::::a    a:::::a 
+O:::::::OOO:::::::Ol::::::ll::::::la::::a    a:::::a m::::m   m::::m   m::::ma::::a    a:::::a 
+ OO:::::::::::::OO l::::::ll::::::la:::::aaaa::::::a m::::m   m::::m   m::::ma:::::aaaa::::::a 
+   OO:::::::::OO   l::::::ll::::::l a::::::::::aa:::am::::m   m::::m   m::::m a::::::::::aa:::a
+     OOOOOOOOO     llllllllllllllll  aaaaaaaaaa  aaaammmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaa
+COMMENT
+
+# Context length
+export OLLAMA_CONTEXT_LENGTH=64000
+export ANTHROPIC_AUTH_TOKEN=ollama
+export ANTHROPIC_API_KEY=""
+export CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC=1
