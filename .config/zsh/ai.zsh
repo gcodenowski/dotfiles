554617efb30e563a18fb08aab175275840e6370c
gcodenowski <gilbertkozanowski@gmail.com>
2026-03-16 00:02:16 +0000

    configured aider for use with qwen

---
+++ .config/zsh/ai.zsh
@@ -0,0 +1,2 @@
+# This is my setup for claude code with ollama
+export OLLAMA_API_BASE=http://localhost:11434
--- .config/zsh/claude.zsh
+++
@@ -1,4 +0,0 @@
-# This is my setup for claude code with ollama
-export ANTHROPIC_BASE_URL=http://localhost:11434/v1
-export ANTHROPIC_API_KEY=""
-export ANTHROPIC_AUTH_TOKEN=ollama
