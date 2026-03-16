bb09e5382db9bbcbd49c5d151bf1b52c9da12ecf
gcodenowski <gilbertkozanowski@gmail.com>
2026-03-16 23:51:01 +0000

    removed legacy_bash

--- .config/sketchybar/legacy_bash/plugins/clock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# == Clock == 
-sketchybar --add item clock right \
-           --set clock update_freq=1 \
-                 script='sketchybar --set $NAME label="$(date "+%H:%M:%S")"' \
-                 icon=􀐫  \
-                 label.width=68 \
-		 padding_left=0  \
--- .config/sketchybar/legacy_bash/plugins/front_app_switched.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# === Logic ===
-if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO" \
-                           icon.background.image="app.$INFO"
-fi
-~                
