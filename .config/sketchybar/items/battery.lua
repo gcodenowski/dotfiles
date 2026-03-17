34884dd78afcff0edf585b832a1528d343b32c37
Gilbert Kozanowski <gilbertkozanowski@example.com>
2026-03-17 12:35:49 +0000

    updated the battery script to match the m3 setup

--- .config/sketchybar/items/battery.lua
+++ .config/sketchybar/items/battery.lua
@@ -1,50 +1,47 @@
--- == BATTERY == 
+-- == BATTERY ==
 
 local icons = require("icons")
-local settings = require("settings")
 
 -- Battery item
 local battery = sbar.add("item", "battery", {
-  padding_left = 8, --to account for the inability to set padding on brackets
-  position = "right",
-  update_freq = 60,
-  label = { 
-	  width = 1,
-	  drawing = off,
-  },
-  click_script = "open 'x-apple.systempreferences:com.apple.Battery-Settings.extension'"
+	position = "right",
+	update_freq = 60,
+	label = {
+		width = 1,
+		drawing = off,
+	},
+	click_script = "open 'x-apple.systempreferences:com.apple.Battery-Settings.extension'",
 })
 
--- Subscribing to the battery event 
-battery:subscribe({"system_woke", "power_source_change"}, function(env)
-  sbar.exec("pmset -g batt", function(info)
-    local found_percent, _, PERCENTAGE = info:find("(%d+)%%")
-    local CHARGING = info:find("AC Power")
-    
-    if not found_percent then
-      return -- exit 0 equivalent
-    end
-    
-    PERCENTAGE = tonumber(PERCENTAGE)
-    local icon = icons.battery._0 -- default
-    
-    if PERCENTAGE >= 90 or PERCENTAGE == 100 then
-      icon = icons.battery._100 
-    elseif PERCENTAGE >= 60 then
-      icon = icons.battery._75
-    elseif PERCENTAGE >= 30 then
-      icon = icons.battery._50
-    elseif PERCENTAGE >= 10 then
-      icon = icons.battery._25
-    end
-    
-    if CHARGING then
-      icon = icons.battery.charging 
-    end
-    
-    battery:set({
-      icon = { string = icon, padding_left = 5 }
-    })
-  end)
-end)
+-- Subscribing to the battery event
+battery:subscribe({ "system_woke", "power_source_change", "battery_update" }, function(env)
+	sbar.exec("pmset -g batt", function(info)
+		local found_percent, _, PERCENTAGE = info:find("(%d+)%%")
+		local CHARGING = info:find("AC Power")
 
+		if not found_percent then
+			return -- exit 0 equivalent
+		end
+
+		PERCENTAGE = tonumber(PERCENTAGE)
+		local icon = icons.battery._0 -- default
+
+		if PERCENTAGE >= 90 or PERCENTAGE == 100 then
+			icon = icons.battery._100
+		elseif PERCENTAGE >= 60 then
+			icon = icons.battery._75
+		elseif PERCENTAGE >= 30 then
+			icon = icons.battery._50
+		elseif PERCENTAGE >= 10 then
+			icon = icons.battery._25
+		end
+
+		if CHARGING then
+			icon = icons.battery.charging
+		end
+
+		battery:set({
+			icon = { string = icon, padding_left = 5 },
+		})
+	end)
+end)
