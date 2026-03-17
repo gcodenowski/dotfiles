b8b4718f8aaf7e5057afb3944a4b012a1e498f42
gcodenowski <gilbertkozanowski@gmail.com>
2026-03-17 00:19:47 +0000

    typo fix

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -18,7 +18,7 @@ alias kittyconfig='cd "$HOME/dotfiles/.config/kitty"'
 alias fastfetchconfig='cd "$HOME/dotfiles/.config/fastfetch"'
 alias sketchybarconfig='cd "$HOME/dotfiles/.config/sketchybar"'
 alias nvimconfig='cd $HOME/dotfiles/.config/nvim"'
-alias wallpapaers='cd "$HOME/Documents/Graphics/Wallpapers"'
+alias wallpapers='cd "$HOME/Documents/Graphics/Wallpapers"'
 alias downloads='cd "$HOME/Downloads"'
 
 # Edit File
