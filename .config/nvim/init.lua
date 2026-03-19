85d7767ecffe864a718c526ceecf8029cc0fd2bf
gcodenowski <gilbertkozanowski@gmail.com>
2026-03-19 22:19:28 +0000

    deleted doc directory inside nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -476,7 +476,7 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
-}, 
+}) 
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
