15af5c0ce55d4cb165e8651129cd8e2718d2a0f5
gcodenowski <gilbertkozanowski@gmail.com>
2026-03-20 14:40:16 +0000

    changed the default width of neotree split

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -20,6 +20,7 @@ return {
         hide_gitignored = false,
       },
       window = {
+        width = 30,
         mappings = {
           ['\\'] = 'close_window',
           ['u'] = 'navigate_up',
