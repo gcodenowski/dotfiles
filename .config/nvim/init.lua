7cbe7ee866c26dfa33c410c754f00ced3d6f5d9b
gcodenowski <gilbertkozanowski@gmail.com>
2026-03-20 13:23:05 +0000

    added python lsp and linter, added C lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -189,10 +189,17 @@ require('lazy').setup {
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'lua-language-server', -- Lua Language server
+        -- Lua
+        'lua-language-server', -- lsp
         'stylua', -- Used to format Lua code
-        'luacheck',
-        -- You can add other tools here that you want Mason to install
+        'luacheck', -- linter
+
+        -- Python
+        'pyright', -- lsp
+        'pyling', -- linter
+
+        -- C
+        'clangd', -- lsp
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
