ba6d9a3c64e34f3929eedac6f46c595fecc92744
gcodenowski <gilbertkozanowski@gmail.com>
2026-03-23 22:28:16 +0000

    added dadbod

---
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'tpope/vim-dadbod',
+    lazy = true,
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      'tpope/vim-dadbod',
+      'kristijanhusak/vim-dadbod-completion',
+    },
+    cmd = { 'DBUI', 'DBUIToggle' },
+    keys = {
+      { '<leader>db', '<cmd>DBUIToggle<cr>', desc = 'Toggle DB UI' },
+    },
+    init = function()
+      vim.g.db_ui_save_location = vim.fn.expand '~/.local/share/db_ui'
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+}
