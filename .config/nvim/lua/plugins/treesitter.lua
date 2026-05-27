00df88145022c43d2e731b4b1580420882d5f1d8
gcodenowski <gilbertkozanowski@gmail.com>
2026-05-27 10:52:05 +0100

    reworked treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,30 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
   config = function()
     vim.treesitter.language.register('sql', 'mysql')
-    local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'typescript', 'python', 'sql' }
-    require('nvim-treesitter').install(filetypes)
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = filetypes,
-      callback = function() vim.treesitter.start() end,
-    })
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'typescript',
+        'javascript',
+        'python',
+        'sql',
+        'java',
+      },
+      auto_install = true, -- installs missing parsers automatically on buffer open
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
   end,
-  build = ':TSUpdate',
 }
