710d664a16123b89a9874358d2e6b9285fd8cbd4
gcodenowski <gilbertkozanowski@gmail.com>
2026-05-31 21:48:33 +0100

    added an alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -27,6 +27,7 @@ alias programming='cd "$HOME/Documents/Programming/"'
 alias university='cd "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Chester/"'
 alias chester='cd "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Chester/"'
 alias dissertation='cd "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Chester/Dissertation"'
+alias tbackend='cd "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Chester/T3/SoftwareStudio/TruckLoaderBackend"'
 
 # Edit File
 alias zshconfig='nvim ~/.zshrc'
