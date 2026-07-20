b8768a46f5c81a5698d6fde4a5504b11fc5faf95
gcodenowski <gilbertkozanowski@gmail.com>
2026-07-20 21:55:34 +0100

    feat: added zdiff to neovim

---
+++ nvim/.config/nvim/lua/plugins/zdiff.lua
@@ -0,0 +1,10 @@
+-- Git Diff viewer in a neovim buffer
+return {
+	"martindur/zdiff.nvim",
+	cmd = "Zdiff",
+	keys = {
+		{ "<leader>zd", "<cmd>Zdiff<cr>", desc = "Zdiff (uncommitted)" },
+		{ "<leader>zD", "<cmd>Zdiff main<cr>", desc = "Zdiff (vs main)" },
+	},
+	opts = {},
+}
