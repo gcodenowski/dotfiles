8f755af4e57a33c489caf193092710ad4da7e7fc
gcodenowski <gilbertkozanowski@gmail.com>
2026-07-21 22:04:48 +0100

    config: added yaml to conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,33 +1,36 @@
 -- Autoformat
 
 return {
-  'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
-  cmd = { 'ConformInfo' },
-  keys = {
-    {
-      '<leader>f',
-      function() require('conform').format { async = true, lsp_format = 'fallback' } end,
-      mode = '',
-      desc = '[F]ormat buffer',
-    },
-  },
-  opts = {
-    notify_on_error = false,
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "[F]ormat buffer",
+		},
+	},
+	opts = {
+		notify_on_error = false,
 
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      python = {'ruff' },
-      typescript = { 'prettier' },
-      javascript = { 'prettier' },
-      json = { 'fixjson' },
-      jsonc = { 'fixjson' },
-      cpp = { 'clang-format' },
-      zsh = { 'shfmt' }, -- My modular shell config files
-      sh = { 'shfmt' },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
-    },
-  },
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff" },
+			typescript = { "prettier" },
+			javascript = { "prettier" },
+			yaml = { "prettier" },
+			json = { "fixjson" },
+			jsonc = { "fixjson" },
+			cpp = { "clang-format" },
+			zsh = { "shfmt" }, -- My modular shell config files
+			sh = { "shfmt" },
+			--
+			-- You can use 'stop_after_first' to run the first available formatter from the list
+			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+	},
 }
