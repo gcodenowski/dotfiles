356b7c591dad0f63637e33038a0dc96293b797de
gcodenowski <gilbertkozanowski@gmail.com>
2026-08-17 17:24:24 +0100

    theme change

--- sketchybar/.config/sketchybar/colors.lua
+++ sketchybar/.config/sketchybar/colors.lua
@@ -4,7 +4,8 @@ return {
 	border = 0x96374f4f,
 	bg_bracket = 0x3d87a9a8,
 	text = 0xffc6dbde,
-	bar = 0x40000000,
+	bar = 0x8a000000,
+	-- bar = 0x40000000, -- old bar colour, good for glassy look
 	transparent = 0x0000000,
 	inactive = 0xff7c8a8c,
 }
