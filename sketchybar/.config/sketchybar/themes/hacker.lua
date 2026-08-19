204153a07a040172aceb32f9c6c42845c2f8df47
gcodenowski <gilbertkozanowski@gmail.com>
2026-08-19 18:30:13 +0100

    some themes added

---
+++ sketchybar/.config/sketchybar/themes/hacker.lua
@@ -0,0 +1,10 @@
+return {
+	accent = 0xff00ff41,
+	bg = 0xa1000000,
+	border = 0x96203030,
+	bg_bracket = 0x3d0d1a0d,
+	text = 0xff33ff66,
+	bar = 0x8a020202,
+	transparent = 0x0000000,
+	inactive = 0xff1c7a2e,
+}
---
+++ sketchybar/.config/sketchybar/themes/vesper.lua
@@ -0,0 +1,10 @@
+return {
+	accent = 0xffFFC799,
+	bg = 0xa1101010,
+	border = 0x96282828,
+	bg_bracket = 0x3d232323,
+	text = 0xffFFFFFF,
+	bar = 0x8a101010,
+	transparent = 0x0000000,
+	inactive = 0xffA0A0A0,
+}
