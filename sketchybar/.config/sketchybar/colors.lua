8c91d78382b3a37382dec9cba03ef36783740e47
gcodenowski <gilbertkozanowski@gmail.com>
2026-08-19 18:30:19 +0100

    modular theme changing

--- sketchybar/.config/sketchybar/colors.lua
+++ sketchybar/.config/sketchybar/colors.lua
@@ -1,11 +1,11 @@
-return {
-	accent = 0xff94dcff,
-	bg = 0xa1000000,
-	border = 0x96374f4f,
-	bg_bracket = 0x3d87a9a8,
-	text = 0xffc6dbde,
-	bar = 0x8a000000,
-	-- bar = 0x40000000, -- old bar colour, good for glassy look
-	transparent = 0x0000000,
-	inactive = 0xff7c8a8c,
-}
+local theme = os.getenv("SKETCHY_THEME")
+if not theme then
+	local f = io.open(os.getenv("HOME") .. "/.config/sketchybar/.theme")
+	if f then
+		theme = f:read("*l")
+		f:close()
+	end
+end
+theme = (theme and theme ~= "") and theme or "default"
+
+return require("themes." .. theme)
